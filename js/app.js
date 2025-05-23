80bb7cc6599acf85152023e93e67320a158c3848
ilafper <ialfper@aulaestudio.es>
2025-05-23 18:48:20 +0200

    bla

---
+++ js/app.js
@@ -0,0 +1,7 @@
+ function showForm(formId) {
+    document.querySelectorAll('.form').forEach(form => form.classList.remove('active'));
+    document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+
+    document.getElementById(formId).classList.add('active');
+    document.querySelector(`.tab[onclick*="${formId}"]`).classList.add('active');
+}
