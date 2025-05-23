5a0eccf4d9d0ccb06ffaa3d3e8e07e682d119a32
ilafper <ialfper@aulaestudio.es>
2025-05-23 20:11:50 +0200

    guardar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
- function showForm(formId) {
+function showForm(formId) {
     document.querySelectorAll('.form').forEach(form => form.classList.remove('active'));
     document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
 
     document.getElementById(formId).classList.add('active');
     document.querySelector(`.tab[onclick*="${formId}"]`).classList.add('active');
 }
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+        delay: 50000,
+    },
+    slidesPerView: 1,
+});
