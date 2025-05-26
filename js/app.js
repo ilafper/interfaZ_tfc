89c51dd9afaf0acff62d2a980359a285f61b87b9
ilafper <ialfper@aulaestudio.es>
2025-05-26 18:31:24 +0200

    aaa

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-function showForm(formId) {
+
+/*cambiar entre login y registro */
+function cambiar(formId) {
     document.querySelectorAll('.form').forEach(form => form.classList.remove('active'));
     document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
 
     document.getElementById(formId).classList.add('active');
     document.querySelector(`.tab[onclick*="${formId}"]`).classList.add('active');
 }
+
+/*tiempo prara cambiar entre imagenes del carrusel */
+
 const swiper = new Swiper(".mySwiper", {
     loop: true,
     autoplay: {
-        delay: 50000,
+        delay: 5000,
     },
     slidesPerView: 1,
 });
