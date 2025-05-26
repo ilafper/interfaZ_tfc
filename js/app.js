f7066a906bcb3bfa72c374115ba429d55c35c7fa
ilafper <ialfper@aulaestudio.es>
2025-05-26 20:18:09 +0200

    colecciones de perfil mas  o menos

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@ function cambiar(formId) {
     document.getElementById(formId).classList.add('active');
     document.querySelector(`.tab[onclick*="${formId}"]`).classList.add('active');
 }
+function cambiar2(formId) {
+    document.querySelectorAll('.form').forEach(form => form.classList.remove('active'));
+    document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+
+    document.getElementById(formId).classList.add('active');
+    document.querySelector(`.tab[onclick*="${formId}"]`).classList.add('active');
+}
+
 
 /*tiempo prara cambiar entre imagenes del carrusel */
 
